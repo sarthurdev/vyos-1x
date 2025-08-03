a06e17d9e6449a60fffd9231697f337580ce355b
sarthurdev <965089+sarthurdev@users.noreply.github.com>
2025-08-03 22:38:02 +0200

    T7557: Update locked user check without spwd module
    `spwd` module was removed in Python 3.13

--- src/op_mode/show_users.py
+++ src/op_mode/show_users.py
@@ -47,13 +47,15 @@ def is_locked(user_name: str) -> bool:
     """Check if a given user has password in shadow db"""
 
     try:
-        import warnings
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore",category=DeprecationWarning)
-            import spwd
-        encrypted_password = spwd.getspnam(user_name)[1]
-        return encrypted_password == '*' or encrypted_password.startswith('!')
-    except (KeyError, PermissionError):
+        with open('/etc/shadow', 'r') as f:
+            for line in f.readlines():
+                args = line.strip().split(':')
+                if len(args) < 2:
+                    continue
+                line_user, password = args[:2]
+                if line_user == user_name:
+                    return password == '*' or password[:1] == '!'
+    except PermissionError:
         print('Cannot access shadow database, ensure this script is run with sufficient permissions')
         sys.exit(1)
 
